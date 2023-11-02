3ed25841bab28188bd95dbf7efe44cfa0d5817e9
czenge25 <czenge25@kentdenver.org>
2023-11-02 12:32:19 -0600

    Started reversing of regular stack

--- PostfixCalculator/src/PostfixCalculator.java
+++ PostfixCalculator/src/PostfixCalculator.java
@@ -6,6 +6,8 @@ FY
 
 import java.util.Scanner;
 
+import static java.lang.Character.isDigit;
+
 public class PostfixCalculator {
     public static void main(String[] args) {
 
@@ -13,5 +15,25 @@ public class PostfixCalculator {
         System.out.println("Please enter an expression in postfix notation (separated by spaces): ");
         String input = s.nextLine();
 
+        Stack stackReg = new Stack();
+        Stack stackInv = new Stack();
+
+        int length = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) != ' ') {
+                stackReg.push(i);
+                length++;
+            }
+        }
+
+        for (int i = 0; i < length; i++) {
+            stackInv.push(stackReg.pop());
+
+        }
+
+        for (int i = 0; i < stackInv.size(); i++) {
+            System.out.println(stackInv.pop());
+        }
     }
 }
