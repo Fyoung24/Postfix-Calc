8906d9f1928ca2f7436d9d743908061b3f568cb7
Bmalley24 <bmalley24@kentdenver.org>
2023-11-07 14:35:58 -0700

    made a txt file reader

--- PostfixCalculator/src/PostfixCalculator.java
+++ PostfixCalculator/src/PostfixCalculator.java
@@ -4,82 +4,123 @@ CZ
 FY
  */
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import javafx.geometry.Pos;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.Scanner;
 
 public class PostfixCalculator {
 
     Stack stackReg;
     String input;
+    int answer;
 
-
-    public void loadTxt () {
+    // Load a postfix expression from a text file and return it as a string
+    public String loadTxt() {
         try {
-            InputStream is = getClass().getResourceAsStream("/txtFile/Postfix_Test.txt");
+            // Open a resource file and read its contents
+            InputStream is = PostfixCalculator.class.getResourceAsStream("/txtFile/Postfix_Test.txt");
             BufferedReader br = new BufferedReader(new InputStreamReader(is));
 
-            String input = br.readLine();
+            // Read the first line of the file as the postfix expression
+            input = br.readLine();
 
+            // Close the file reader
             br.close();
 
         } catch (Exception e) {
-
+            // Handle any exceptions that may occur when reading the file
         }
 
-
+        return input;
     }
 
-
-    public static int Calculations (Stack stackReg, String input) {
+    // Perform calculations on the postfix expression and return the result
+    public int Calculations(Stack stackReg, String input) {
         int firstNum = 0;
         int secondNum = 0;
         String[] array = null;
 
-        for(int i = 0; i < input.length(); i++) {
+        // Split the input string into an array of tokens separated by spaces
+        for (int i = 0; i < input.length(); i++) {
             array = input.split(" ");
         }
+
         for (int i = 0; i < array.length; i++) {
-            if (array[i].equals("+") || array[i].equals("-") || array[i].equals("*") || array[i].equals("/") ) {
+            if (array[i].equals("+") || array[i].equals("-") || array[i].equals("*") || array[i].equals("/") || array[i].equals("%")) {
                 firstNum = (int) stackReg.pop();
                 secondNum = (int) stackReg.pop();
-                switch(array[i]) {
+                switch (array[i]) {
                     case "+": {
+                        // Perform addition and push the result onto the stack
                         int newNum = firstNum + secondNum;
                         stackReg.push(newNum);
                         break;
                     }
 
                     case "-": {
+                        // Perform subtraction and push the result onto the stack
                         int newNum = firstNum - secondNum;
                         stackReg.push(newNum);
                         break;
                     }
 
                     case "*": {
+                        // Perform multiplication and push the result onto the stack
                         int newNum = firstNum * secondNum;
                         stackReg.push(newNum);
                         break;
                     }
 
                     case "/": {
-                        int newNum = firstNum / secondNum;
+                        // Perform division and push the result onto the stack
+                        int newNum = secondNum / firstNum;
+                        stackReg.push(newNum);
+                        break;
+                    }
+
+                    case "%": {
+                        // Perform mod and push the result onto the stack
+                        int newNum = secondNum % firstNum;
                         stackReg.push(newNum);
                         break;
                     }
                 }
             } else {
+                // If the token is not an operator, convert it to an integer and push it onto the stack
                 stackReg.push(Integer.parseInt(array[i]));
             }
         }
-        return (int) stackReg.peek();
 
+        // Get the final answer from the stack
+        answer = (int) stackReg.peek();
+        return answer;
     }
 
-}
-// input.charAt(i)
-// Integer.parseInt(input.substring(i))
+    // Log the result to a text file
+    public void log(int answer) throws IOException {
+        String newAnswer = String.valueOf(answer);
 
-// String numbers [] = line.split(" ");
-// int num = Integer.parseInt(numbers[col]);
+        // Try to write the result to a log file
+        try {
+            // Create a FileWriter to write to the "log.txt" file
+            FileWriter fWriter = new FileWriter("log.txt");
+
+            // Write the result along with the original expression to the file
+            fWriter.write("This is the answer to " + input + ": " + newAnswer);
+
+            // Print the result to the console
+            System.out.println(newAnswer);
+
+            // Close the file writer
+            fWriter.close();
+        } catch (IOException e) {
+            // Handle exceptions if there's an issue with writing to the file
+            System.out.print(e.getMessage());
+        }
+
+    }
+}
